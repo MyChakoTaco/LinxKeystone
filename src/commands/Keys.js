0a261814fdfa6393b08f8aefbde57eb61f56e2e5
MyChakoTaco <charles.k.hsueh@gmail.com>
2021-10-06 14:20:02 -0700

    feature: Add filtering and text display

--- src/commands/Keys.js
+++ src/commands/Keys.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { getEmbed } = require('../LinxKeyEmbeder.js')
+const { getKeysReply } = require('../LinxKeysReply.js')
 
 const KEYS_FILEPATH = 'data/CommandKeys.json'
 
@@ -7,10 +7,26 @@ let _PrevKeysInteraction
 
 const Keys = async (interaction) => {
   const sort = interaction.options.getString('sort')
+  const filter = interaction.options.getString('filter')
+  const display = interaction.options.getString('display')
+    ? interaction.options.getString('display')
+    : 'embed'
 
   await deletePrevKeysInteractions()
+    .catch((e) => {
+      console.warn('There was an error deleting previous messages: ' + e)
+    })
     .then(() => {
-      interaction.reply({ embeds: [getEmbed(sort)] })
+      const reply = getKeysReply({
+        sort: sort,
+        filter: filter,
+        display: display,
+      })
+
+      interaction.reply(reply)
+    })
+    .catch((e) => {
+      console.warn('There was an error replying to command: ' + e)
     })
     .then(() => {
       _PrevKeysInteraction = interaction
@@ -18,7 +34,7 @@ const Keys = async (interaction) => {
 }
 
 async function deletePrevKeysInteractions() {
-  if (_PrevKeysInteraction) _PrevKeysInteraction.deleteReply()
+  // if (_PrevKeysInteraction) _PrevKeysInteraction.deleteReply()
 }
 
 module.exports = { Keys }
